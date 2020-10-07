6854fd340d568f60471cd65b90978e9be5a2988a
versus07 <versus07@gmail.com>
2020-10-07 09:34:37 +0300

    Task with not unique values in the array

---
+++ c#/NotUniqInArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NotUniqInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr=new int[10] {5,4,2,3,4,6,8,0,1,3};
+            Console.WriteLine("Исходный массив:");
+            foreach (int i in Arr)
+            {
+                Console.Write(Arr[i] + " ");
+            }
+            Console.WriteLine();
+
+
+            Array.Sort(Arr);
+            Console.WriteLine("Отсортированный массив:");
+            foreach (int i in Arr)
+            {
+                Console.Write(Arr[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Неуникальные значения:");
+            for (int i=0;i<10;i++)
+            {
+                if (Array.IndexOf(Arr,Arr[i],i+1)!=-1)
+                {
+                    Console.Write(Arr[i]+" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
