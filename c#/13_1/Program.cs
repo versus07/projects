f689a9caddbd6d1a01c0061bb48c7327edf17ad8
versus07 <versus07@gmail.com>
2020-10-13 15:33:24 +0300

    update for 13_10_2020

--- c#/13_1/Program.cs
+++ c#/13_1/Program.cs
@@ -13,8 +13,10 @@ namespace _13_1
     struct Article
     {
         public int ProductId;
-       public string ProductName;
+        public string ProductName;
         public double ProductPrice;
+        
+        public ArticleType articletype;
     }
     
     struct Client
@@ -25,12 +27,44 @@ namespace _13_1
         public string ClientPhone;
         public int ClientOrdersCount;
         public double ClientAllOrdersSum;
+
+        public ClientType clienttype;
+    }
+
+    struct Request
+    {
+        public int RequestID;
+        public string Client;
+        public DateTime RequestDate;
+        public string ItemList;
+        public PayType paytype;
+        public double RequestSum { get; set; }  //Вычисляемое свойство 
+        
     }
 
     struct RequestItem
     {
+       
         public string ItemName;
+        public int ItemCount;
+        
 
+        
+    }
+
+    enum ArticleType
+    {
+        Electronics, FrozenFood, Food, DomesticChemistry,BuildingMaterials, Petrol
+    }
+
+    enum ClientType
+    {
+        NotImportant, Important, VeryImportant
+    }
+
+    enum PayType
+    {
+        Cash, CreditCard
     }
 
     class Program
@@ -41,9 +75,32 @@ namespace _13_1
             a.ProductId = 1;
             a.ProductName = "телевизор";
             a.ProductPrice = 3500;
+            a.articletype = ArticleType.Electronics;
+            Console.WriteLine($"Код товара - {a.ProductId}\nТип товара - {a.articletype}\nНазвание товара - {a.ProductName}\nЦена продукта - {a.ProductPrice}");
 
-            Console.WriteLine($"Код товара - {a.ProductId}\nНазвание товара - {a.ProductName}\nЦена продукта - {a.ProductPrice}");
-            
+            Console.WriteLine("-------------------------------------");
+
+            Client c = new Client();
+            c.ClientId = 1;
+            c.ClientName = "Ivanov";
+            c.ClientAddress = "Moscow";
+            c.ClientPhone = "+74965432345";
+            c.clienttype = ClientType.Important;
+            c.ClientOrdersCount = 5;
+
+            Console.WriteLine($"Код клиента - {c.ClientId}\nФИО клиента - {c.ClientName}\nАдрес клиента - {c.ClientAddress}\nТелефон клиента - {c.ClientPhone}\nТип клиента - {c.clienttype}");
+
+            Console.WriteLine("-------------------------------------");
+
+            RequestItem ri = new RequestItem();
+            ri.ItemCount = 4;
+            ri.ItemName = "телевизор";
+
+
+            Request r = new Request();
+            r.RequestSum = a.ProductPrice * ri.ItemCount;
+            Console.WriteLine($"Сумма заказа - {r.RequestSum} руб.");
+
         }
     }
 }
