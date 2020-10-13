b615fcdc369ea30d8e133d2a807ea000f81e95f1
versus07 <versus07@gmail.com>
2020-10-13 08:38:27 +0300

    update for 13_10

---
+++ c#/13_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Описать стуктуру Article, содержащую следующие поля:
+ * код товара
+ * название товара
+ * цена товара
+ */
+namespace _13_1
+{
+    struct Article
+    {
+        public int ProductId;
+       public string ProductName;
+        public double ProductPrice;
+    }
+    
+    struct Client
+    {
+        public int ClientId;
+        public string ClientName;
+        public string ClientAddress;
+        public string ClientPhone;
+        public int ClientOrdersCount;
+        public double ClientAllOrdersSum;
+    }
+
+    struct RequestItem
+    {
+        public string ItemName;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Article a = new Article();
+            a.ProductId = 1;
+            a.ProductName = "телевизор";
+            a.ProductPrice = 3500;
+
+            Console.WriteLine($"Код товара - {a.ProductId}\nНазвание товара - {a.ProductName}\nЦена продукта - {a.ProductPrice}");
+            
+        }
+    }
+}
---
+++ c#/13_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("13_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("13_1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("51c957b0-8ab3-471f-9606-aa8256a9886e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
