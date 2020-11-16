ffc42ee2388e15ee318f6cce6f9fa48ad1e33724
versus07 <versus07@gmail.com>
2020-11-16 16:25:14 +0300

    vplayer_16112020

--- c#/vplayer/WindowsFormsApp1/Form1.Designer.cs
+++ c#/vplayer/WindowsFormsApp1/Form1.Designer.cs
@@ -28,11 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolTipAddButton = new System.Windows.Forms.ToolTip(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.toolTipSaveButton = new System.Windows.Forms.ToolTip(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -45,21 +57,22 @@
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Видео (*.avi;*.mp4;*.mpeg;*.mpg;*.wmv)|*.avi;*.mp4;*.mpeg;*.mpg;*.wmv";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(621, 220);
+            this.button1.Location = new System.Drawing.Point(536, 297);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.Size = new System.Drawing.Size(24, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "OPEN VIDEO";
+            this.button1.Text = "+";
+            this.toolTipAddButton.SetToolTip(this.button1, "Добавить видео в плейлист");
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(614, 317);
+            this.button2.Location = new System.Drawing.Point(147, 366);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -69,13 +82,86 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(617, 366);
+            this.button3.Location = new System.Drawing.Point(228, 366);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
             this.button3.Text = "PLAY";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(536, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(182, 238);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(41, 315);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(320, 45);
+            this.trackBar1.TabIndex = 5;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Enabled = false;
+            this.trackBar2.LargeChange = 50;
+            this.trackBar2.Location = new System.Drawing.Point(378, 187);
+            this.trackBar2.Maximum = 0;
+            this.trackBar2.Minimum = -5000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 104);
+            this.trackBar2.SmallChange = 2;
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.TickFrequency = 500;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.toolTipAddButton.SetToolTip(this.trackBar2, "Громкость");
+            this.trackBar2.Value = -2000;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(639, 297);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(27, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "S";
+            this.toolTipAddButton.SetToolTip(this.button4, "Сохранить плейлист");
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "List Files (*.lst)|*.lst";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(566, 297);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(24, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "-";
+            this.toolTipAddButton.SetToolTip(this.button5, "Удалить видео из плейлиста");
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(672, 297);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(27, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "L";
+            this.toolTipAddButton.SetToolTip(this.button6, "Загрузить плейлист");
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
@@ -84,14 +170,23 @@
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -102,6 +197,15 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolTip toolTipAddButton;
+        private System.Windows.Forms.ToolTip toolTipSaveButton;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- c#/vplayer/WindowsFormsApp1/Form1.cs
+++ c#/vplayer/WindowsFormsApp1/Form1.cs
@@ -13,7 +13,8 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         Video MyVideo;
-        
+        string VideoName;
+
         public Form1()
         {
             InitializeComponent();
@@ -26,56 +27,118 @@ namespace WindowsFormsApp1
         }
        
 
+        //Кнопка Добавить в плейлист
         private void button1_Click(object sender, EventArgs e)
         {
+            
+
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 MyVideo = new Video(openFileDialog1.FileName);
 
+                VideoName = openFileDialog1.FileName;
+                listBox1.Items.Add(VideoName);
+
+
                
-
-
-
-                if (MyVideo.Playing == true)
-                {
-                    MyVideo.Stop();
-                    MyVideo.Audio.Stop();
-                    MyVideo.Dispose();
-                }
-                else
-                {
-
-                    MyVideo.Open(openFileDialog1.FileName);
-
-                    
-                    MyVideo.Owner = panel1;
-                    MyVideo.Owner.Width = 320;
-                    MyVideo.Owner.Height = 240;
-                    
-                    MyVideo.Play();
-
-                }
 
             }
         }
-       
 
+
+        //Кнопка Удалить из плейлиста
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+
+        }
+
+        //Кнопка STOP
         private void button2_Click(object sender, EventArgs e)
         {
             if (MyVideo.Playing == true)
             {
+                trackBar2.Enabled = false;
                 MyVideo.Stop();
-                MyVideo.Audio.Stop();
-                MyVideo.Dispose();
+               
+               // MyVideo.Dispose();
             }
         }
 
+        //Кнопка PLAY
         private void button3_Click(object sender, EventArgs e)
         {
-            if (MyVideo.Playing==false)
+
+            if (VideoName != null)
             {
+                MyVideo.Open(VideoName);
+                MyVideo.Owner = panel1;
+                MyVideo.Owner.Width = 320;
+                MyVideo.Owner.Height = 240;
+                
+                trackBar2.Enabled = true;
                 MyVideo.Play();
             }
+            
+        }
+
+        //Выбор видео из плейлиста
+        public void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                VideoName = listBox1.SelectedItem.ToString();
+            }
+        }
+
+        //Двойной щелчок по файлу в списке
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                VideoName = listBox1.SelectedItem.ToString();
+                if (VideoName != null)
+                {
+                    MyVideo.Open(VideoName);
+                    MyVideo.Owner = panel1;
+                    MyVideo.Owner.Width = 320;
+                    MyVideo.Owner.Height = 240;
+
+                    trackBar2.Enabled = true;
+                    MyVideo.Play();
+                }
+            }
+        }
+
+        //Кнопка сохранить плейлист
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                using (System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(saveFileDialog1.FileName))
+                {
+                    foreach (var item in listBox1.Items)
+                        SaveFile.WriteLine(item.ToString());
+                }
+            }
+        }
+
+        //Кнопка Загрузить плейлист
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            
+            
+                MyVideo.Audio.Volume = trackBar2.Value;
+            
         }
     }
 
