4bc3499ed7941fdf324b65d8311d389246405c1a
versus07 <versus07@gmail.com>
2020-11-18 16:34:57 +0300

    dict_18112020

---
+++ c#/DictApp/EnRuDict.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DictApp
+{
+    class EnRuDict
+    {
+        protected Dictionary<string, string> EnRu;
+
+        //Создание словаря
+        public EnRuDict()
+        {
+            EnRu = new Dictionary<string, string>();
+
+        }
+        
+        //добавление слова
+        public void WordEnRuAdd()
+        {
+            string word, translation;
+            
+            Console.WriteLine("Введите английское слово ");
+            word=Console.ReadLine();
+            Console.WriteLine("Введите его перевод ");
+            translation = Console.ReadLine();
+            EnRu.Add(word, translation);
+        }
+
+        //удаление слова
+
+
+        //печать словаря
+        public void PrintEnRuDict()
+        {
+            foreach (KeyValuePair<string, string> keyValue in EnRu)
+            {
+                Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+            }
+        }
+
+        //поиск слова
+
+
+        //сохранение словаря в файл
+
+
+        //загрузка словаря из файла
+
+    }
+}
